61de9f79e5c23ede4abc980abf56c833a42c05b6
AarinK <aarin.kachroo@gmail.com>
2023-07-24 23:29:57 +0530

    msg

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Animated, Easing } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import SplashScreenComponent from './screens/splashscreen';
-import Dashboard from './screens/dashboard';
 
 const App = () => {
-  const [showSplash, setShowSplash] = useState(true);
-  const [fadeAnimSplash] = useState(new Animated.Value(1));
-  const [delayOver, setDelayOver] = useState(false); 
-
-  useEffect(() => {
-    setTimeout(() => {
-      setDelayOver(true);
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    if (delayOver) {
-      Animated.timing(fadeAnimSplash, {
-        toValue: 0,
-        duration: 1000, 
-        useNativeDriver: true,
-        easing: Easing.ease,
-      }).start(() => {
-        setShowSplash(false);
-      });
-    }
-  }, [delayOver]);
-
   return (
-    <>
-      {showSplash ? (
-        <Animated.View style={{ flex: 1, opacity: fadeAnimSplash }}>
-          <SplashScreenComponent />
-        </Animated.View>
-      ) : (
-        <Animated.View style={{ flex: 1, opacity: 1 }}>
-          <Dashboard />
-        </Animated.View>
-      )}
-    </>
+    <View style={{ flex: 1 }}>
+      <SplashScreenComponent />
+    </View>
   );
 };
 
--- screens/splashscreen.js
+++ screens/splashscreen.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
+import React, { useEffect, useRef } from 'react';
+import { View, Image, StyleSheet, Animated } from 'react-native';
 
 const SplashScreenComponent = () => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000, 
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
 
   return (
     <View style={styles.container}>
-    
-      <Image
-        source={require('../assets/splash.png')}
-        style={styles.splashImage}
+      <Image source={require('../assets/welcomebg.png')} style={styles.BgImage} />
+      <Image source={require('../assets/flower1.png')} style={styles.BigImage1} resizeMode="contain" />
+      <Image source={require('../assets/flower2.png')} style={styles.SmallImage1} resizeMode="contain" />
+      <Image source={require('../assets/flower5.png')} style={styles.SmallImage2} resizeMode="contain" />
+      <Image source={require('../assets/flower3.png')} style={styles.SmallImage3} resizeMode="contain" />
+      <Image source={require('../assets/flower4.png')} style={styles.BigImage2} resizeMode="contain" />
+      <Image source={require('../assets/flower6.png')} style={styles.BigImage3} resizeMode="contain" />
+      <Animated.Image
+        source={require('../assets/hellotext.png')}
+        style={[styles.Text, { opacity: fadeAnim }]}
+        resizeMode="contain"
       />
     </View>
   );
@@ -21,13 +35,60 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-        backgroundColor: '#2D1D13', 
-
+    backgroundColor: '#54412E',
   },
-  splashImage: {
+  BgImage: {
     width: '100%',
     height: '100%',
-    // resizeMode: 'contain',
+  },
+  SmallImage1: {
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    top: '82%',
+    left: '52%',
+  },
+  SmallImage2: {
+    width: 45,
+    height: 60,
+    position: 'absolute',
+    top: '5%',
+    left: '26%',
+  },
+  SmallImage3: {
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    top: '10%',
+    left: '71%',
+  },
+  BigImage2: {
+    width: 70,
+    height: 100,
+    position: 'absolute',
+    top: '25%',
+    left: '0%',
+  },
+  BigImage1: {
+    width: 70,
+    height: 100,
+    position: 'absolute',
+    top: '89%',
+    left: '85%',
+  },
+  BigImage3: {
+    width: 70,
+    height: 130,
+    position: 'absolute',
+    top: '88%',
+    left: '0%',
+  },
+  Text: {
+    width: 200,
+    height: 50,
+    position: 'absolute',
+    top: '32%',
+    left: '35%',
   },
 });
 
